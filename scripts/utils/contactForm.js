fa41da8f03be07837b9b65775e5a0ab432d1b1ae
Catalina Segura <catalina.segura@vadesecure.com>
2024-01-15 12:52:57 +0100

    feat: contact form implementation

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,93 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+
+// DOM Elements (modal)
+const contactBtn = document.querySelector(".contact_button");
+const modalBg = document.getElementById("contact_modal");
+const closeBtn = document.querySelector(".close_button");
+const close = document.querySelector(".submit_button");
+
+
+// Launch open modal event
+contactBtn.addEventListener("click", displayModal);
+
+// Display modal form
+function displayModal() {    
+	modalBg.style.display = "block";
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+// Launch close modal event
+closeBtn.addEventListener("click", closeModal);
+close.addEventListener("click", closeModal);
+
+// Close modal form
+function closeModal() {    
+	modalBg.style.display = "none";
 }
+
+// Implement form inputs
+// Create field class in order to simplify field's validation
+class Field {
+	// Define details
+	constructor(docName, regex, docErrorMsg, errorMsg) {
+	  this.docName = docName;
+	  this.regex = regex;
+	  this.docErrorMsg = docErrorMsg;
+	  this.errorMsg = errorMsg;
+	}
+	// Define methods
+	// Add an event to listen to the changes and check the inputs 
+	addEvent() {
+	  this.docName.addEventListener("focusout", (event) => {
+		event.preventDefault();
+		this.checkField();
+	  })
+	}
+	// Check the field and display an error message if needed
+	checkField() {
+	  let valid = this.regex.test(this.docName.value);
+	  if (valid === false) {
+		this.docName.style.border = "2px outset red";
+		this.docErrorMsg.textContent = this.errorMsg;
+		return false
+	  } else {
+		this.docName.style.border = "none";
+		this.docErrorMsg.textContent = "";
+		return true
+	  }
+	}
+  }
+  
+  // Create field object (firstName, lastName, email)
+  // Add eventListener (firstName, lastName, email)
+  let firstName = new Field(
+	document.getElementById("firstName"),
+	new RegExp(/^(?=.*[A-Za-z].*[a-zA-Z])[A-Za-z\é\è\ê\ë\ï\œ\-\s]+$/),
+	document.getElementById("firstNameErrorMsg"),
+	`Veuillez entrer un prénom valide`
+  );
+  firstName.addEvent();
+  
+  let lastName = new Field(
+	document.getElementById("lastName"),
+	new RegExp(/^(?=.*[A-Za-z].*[a-zA-Z])[A-Za-z\é\è\ê\ë\ï\œ\-\s]+$/),
+	document.getElementById("lastNameErrorMsg"),
+	`Veuillez entrer un nom valide`
+  );
+  lastName.addEvent();
+  
+  let email = new Field(
+	document.getElementById("email"),
+	new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+[@]{1}[A-Za-z0-9.-]+\.[\w-]{2,4}$/),
+	document.getElementById("emailErrorMsg"),
+	`Veuillez entrer une adresse mail valide`
+  );
+  email.addEvent();
+
+// Implement submit btn  
+// Recover the DOM element for the "submitBtn"
+const submitBtn = document.getElementById("submitBtn");
+
+// // Listen to the click and check the inputs
+submitBtn.addEventListener("submit", (event) => {
+  	event.preventDefault();  
+	document.forms[0].reset()
+})
