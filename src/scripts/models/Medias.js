a1566772c8fb49a4acbd5d321fb0c72ba1eeceba
Catalina Segura <catalina.segura@vadesecure.com>
2024-02-29 18:39:17 +0100

    chore: add eslint

--- src/scripts/models/Medias.js
+++ src/scripts/models/Medias.js
@@ -1,252 +1,250 @@
 // Index to handle lightbox
 let currentMediaIndex = 0
 
-class Medias {    
-    constructor(id, photographerId, title, source, likes, date, price) {
-        this.id = id
-        this.photographerId = photographerId
-        this.title = title
-        this.source = source
-        this.likes = likes
-        this.date = date
-        this.price = price
+class Medias {
+  constructor (id, photographerId, title, source, likes, date, price) {
+    this.id = id
+    this.photographerId = photographerId
+    this.title = title
+    this.source = source
+    this.likes = likes
+    this.date = date
+    this.price = price
+  }
+
+  displayMedias () {
+    const gallerySection = document.querySelector('.photographer_gallery_medias')
+
+    // Create an "article" tag dedicated to a media
+    const mediaArticle = document.createElement('article')
+    mediaArticle.className = 'gallery_image'
+    mediaArticle.setAttribute('aria-label', 'Galerie d\'images du photographe')
+    mediaArticle.setAttribute('data-date', this.date)
+    mediaArticle.setAttribute('data-likes', this.likes)
+    mediaArticle.setAttribute('data-source', this.source)
+    mediaArticle.setAttribute('data-title', this.title)
+    mediaArticle.setAttribute('data-is-img', this instanceof ImageMedia)
+    gallerySection.appendChild(mediaArticle)
+
+    // Create an "a" tag to make the link to a media
+    const mediaLink = document.createElement('a')
+    mediaLink.className = 'media_link'
+    mediaLink.href = ''
+    mediaLink.setAttribute('aria-label', `Lien vers la vue rapprochée de l'œuvre intitulé ${this.title} qui compte avec ${this.likes} likes`)
+    mediaArticle.appendChild(mediaLink)
+
+    mediaLink.appendChild(this.getSource())
+
+    // Launch open lightbox event
+    mediaLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      this.displayLightbox()
+      const firstElement = document.querySelector('.lightbox_media')
+      firstElement.focus()
+    })
+
+    // Create a "span" tag for each media info
+    const mediaInfo = document.createElement('span')
+    mediaArticle.appendChild(mediaInfo)
+
+    // Create a "h3" tag for each media title
+    const mediaTitle = document.createElement('h3')
+    mediaTitle.textContent = this.title
+    mediaInfo.appendChild(mediaTitle)
+
+    // Create a "div" tag for the likes ans the icon
+    const mediaLikesInfo = document.createElement('div')
+    mediaInfo.appendChild(mediaLikesInfo)
+
+    // Create a "p" tag for each media likes
+    const mediaLikes = document.createElement('p')
+    mediaLikes.className = 'number_of_likes'
+    mediaLikes.setAttribute('aria-label', 'likes')
+    mediaLikes.textContent = this.likes
+    mediaLikesInfo.appendChild(mediaLikes)
+
+    const mediaLikesBtn = document.createElement('button')
+    mediaLikesBtn.className = 'likes_icon'
+    mediaLikesBtn.setAttribute('type', 'button')
+    mediaLikesBtn.setAttribute('data-liked', 'unliked')
+    mediaLikesBtn.setAttribute('aria-label', 'Cliquez pour liker ce media')
+    mediaLikesInfo.appendChild(mediaLikesBtn)
+
+    // Create a "I" tag for the icon
+    const mediaLikesIcon = document.createElement('i')
+    mediaLikesIcon.classList.add('fa-solid')
+    mediaLikesIcon.classList.add('fa-heart')
+    mediaLikesBtn.appendChild(mediaLikesIcon)
+  }
+
+  displayLightbox () {
+    const lightboxBg = document.getElementById('lightbox_modal')
+    lightboxBg.style.display = 'block'
+    const lightboxSlide = document.querySelector('.lightbox_media')
+    lightboxSlide.setAttribute('aria-label', `vue rapprochée de l'œuvre intitulé ${this.title}`)
+    lightboxSlide.appendChild(this.getLightboxSource())
+
+    // Create a "h3" tag for each media title
+    const imageTitle = document.createElement('h3')
+    imageTitle.className = 'slide_title'
+    imageTitle.textContent = this.title
+    lightboxSlide.appendChild(imageTitle)
+
+    // START OF LIGHTBOX CONTROLS //
+    // Launch close lightbox event
+    const close = document.getElementById('close_lightbox')
+    close.addEventListener('click', (e) => {
+      e.preventDefault()
+      lightboxSlide.innerHTML = ''
+      lightboxBg.style.display = 'none'
+    })
+
+    // Launch close lightbox event with escape key
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        lightboxSlide.innerHTML = ''
+        lightboxBg.style.display = 'none'
+      }
+    })
+
+    // Launch next lightbox event
+    const showNext = document.getElementById('right')
+    showNext.addEventListener('click', (e) => {
+      e.preventDefault()
+      this.navigateLightbox(1)
+    })
+
+    // Launch prev lightbox event
+    const showPrevious = document.getElementById('left')
+    showPrevious.addEventListener('click', (e) => {
+      e.preventDefault()
+      this.navigateLightbox(-1)
+    })
+
+    // Navigate through images with arrow keys
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'ArrowRight') {
+        // right arrow key
+        e.preventDefault()
+        this.navigateLightbox(1)
+      } else if (e.key === 'ArrowLeft') {
+        // left arrow key
+        e.preventDefault()
+        this.navigateLightbox(-1)
+      }
+    })
+    // END OF LIGHTBOX CONTROLS //
+  }
+
+  navigateLightbox (direction) {
+    // Get the list of medias from the DOM
+    const galleryMedias = document.querySelector('.photographer_gallery_medias')
+    const items = galleryMedias.childNodes
+    const itemsList = []
+
+    // eslint-disable-next-line prefer-const
+    for (let i in items) {
+    // get rid of the whitespace text nodes
+      if (items[i].nodeType === 1) {
+        itemsList.push(items[i])
+      }
     }
 
-    displayMedias() {
-        const gallerySection = document.querySelector(".photographer_gallery_medias")
+    // Update the current index based on the navigation direction
+    const currentMedia = itemsList[currentMediaIndex += direction]
 
-        // Create an "article" tag dedicated to a media
-        const mediaArticle = document.createElement("article")
-        mediaArticle.className = "gallery_image"
-        mediaArticle.setAttribute('aria-label', `Galerie d'images du photographe`)
-        mediaArticle.setAttribute('data-date', this.date)
-        mediaArticle.setAttribute('data-likes', this.likes)
-        mediaArticle.setAttribute('data-source', this.source)
-        mediaArticle.setAttribute('data-title', this.title)
-        mediaArticle.setAttribute('data-is-img', this instanceof ImageMedia);
-        gallerySection.appendChild(mediaArticle)
-
-        // Create an "a" tag to make the link to a media
-        const mediaLink = document.createElement("a")
-        mediaLink.className = "media_link"
-        mediaLink.href = ``
-        mediaLink.setAttribute('aria-label', `Lien vers la vue rapprochée de l'œuvre intitulé ${this.title} qui compte avec ${this.likes} likes`)
-        mediaArticle.appendChild(mediaLink)
-
-        mediaLink.appendChild(this.getSource())
-
-        // Launch open lightbox event
-        mediaLink.addEventListener("click", (e) => {
-            e.preventDefault()
-            this.displayLightbox()
-            const firstElement = document.querySelector(".lightbox_media")
-	        firstElement.focus()
-
-        })
-
-        // Create a "span" tag for each media info
-        const mediaInfo = document.createElement("span")
-        mediaArticle.appendChild(mediaInfo)
-
-        // Create a "h3" tag for each media title
-        const mediaTitle = document.createElement("h3")        
-        mediaTitle.textContent = this.title
-        mediaInfo.appendChild(mediaTitle)
-
-        // Create a "div" tag for the likes ans the icon
-        const mediaLikesInfo = document.createElement("div")
-        mediaInfo.appendChild(mediaLikesInfo)
-
-        // Create a "p" tag for each media likes
-        const mediaLikes = document.createElement("p")
-        mediaLikes.className = "number_of_likes"
-        mediaLikes.setAttribute('aria-label', 'likes')
-        mediaLikes.textContent = this.likes
-        mediaLikesInfo.appendChild(mediaLikes)
-
-        const mediaLikesBtn = document.createElement("button")
-        mediaLikesBtn.className = "likes_icon"
-        mediaLikesBtn.setAttribute('type', 'button')
-        mediaLikesBtn.setAttribute('data-liked', 'unliked')
-        mediaLikesBtn.setAttribute('aria-label', 'Cliquez pour liker ce media')
-        mediaLikesInfo.appendChild(mediaLikesBtn)
-        
-        // Create a "I" tag for the icon
-        const mediaLikesIcon = document.createElement("i")        
-        mediaLikesIcon.classList.add('fa-solid')
-        mediaLikesIcon.classList.add('fa-heart')
-        mediaLikesBtn.appendChild(mediaLikesIcon)
-    } 
-
-    displayLightbox() {   
-        const lightboxBg = document.getElementById("lightbox_modal") 
-        lightboxBg.style.display = "block"
-        const lightboxSlide = document.querySelector(".lightbox_media")
-        lightboxSlide.setAttribute('aria-label', `vue rapprochée de l'œuvre intitulé ${this.title}`)
-        lightboxSlide.appendChild(this.getLightboxSource())
-
-        //Create a "h3" tag for each media title
-        const imageTitle = document.createElement("h3")
-        imageTitle.className = "slide_title"       
-        imageTitle.textContent = this.title
-        lightboxSlide.appendChild(imageTitle)
-
-        // START OF LIGHTBOX CONTROLS //
-        // Launch close lightbox event
-        const close = document.getElementById("close_lightbox")
-        close.addEventListener("click", (e) => {
-            e.preventDefault()
-            lightboxSlide.innerHTML = ''
-            lightboxBg.style.display = "none";
-        })
-
-        // Launch close lightbox event with escape key
-        document.addEventListener('keydown', (e) => {
-	        if (e.key === 'Escape') {
-	            lightboxSlide.innerHTML = ''
-                lightboxBg.style.display = "none"
-	        }
-        })
-
-        // Launch next lightbox event
-        const showNext = document.getElementById("right")
-        showNext.addEventListener("click", (e) => {
-            e.preventDefault()
-            this.navigateLightbox(1)
-        })
-
-        // Launch prev lightbox event
-        const showPrevious = document.getElementById("left")
-        showPrevious.addEventListener("click", (e) => {
-            e.preventDefault()
-            this.navigateLightbox(-1)
-        })
-
-        // Navigate through images with arrow keys
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'ArrowRight') { 
-                // right arrow key
-                e.preventDefault()
-                this.navigateLightbox(1)    
-            } else if (e.key === 'ArrowLeft') {
-                // left arrow key 
-                e.preventDefault()
-                this.navigateLightbox(-1)    
-            }
-        })
-        // END OF LIGHTBOX CONTROLS //       
-    }    
-
-    navigateLightbox(direction) {
-        // Get the list of medias from the DOM
-        const galleryMedias = document.querySelector(".photographer_gallery_medias")
-        const items = galleryMedias.childNodes    
-        const itemsList = []
-
-        for (let i in items) {
-            if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-                itemsList.push(items[i])
-            }
-        }
-
-        console.log(itemsList)
-    
-        // Update the current index based on the navigation direction
-        let currentMedia = itemsList[currentMediaIndex += direction ]
-
-        // Wrap around to the first/last image if necessary
-        if (currentMediaIndex < 0) {
-            currentMediaIndex = itemsList.length - 1
-        } else if (currentMediaIndex >= itemsList.length) {
-            currentMediaIndex = 0
-        }
-        this.updateLightbox(currentMedia)
+    // Wrap around to the first/last image if necessary
+    if (currentMediaIndex < 0) {
+      currentMediaIndex = itemsList.length - 1
+    } else if (currentMediaIndex >= itemsList.length) {
+      currentMediaIndex = 0
     }
+    this.updateLightbox(currentMedia)
+  }
 
-    updateLightbox(currentMedia) {
-        let source = currentMedia.getAttribute('data-source')
-        console.log(source)
-        let title = currentMedia.getAttribute('data-title')
-        console.log(title)
-        let isImage = currentMedia.getAttribute('data-is-img')
-        console.log(isImage)
+  updateLightbox (currentMedia) {
+    const source = currentMedia.getAttribute('data-source')
+    const title = currentMedia.getAttribute('data-title')
+    const isImage = currentMedia.getAttribute('data-is-img')
 
-        // Set the image source and title based on the current image index        
-        if (isImage === 'true') {
-            // Get the original element
-            const slideSource = document.querySelector(".slide_source")
-            // Create a new element with a img tag name
-            const slideChangeToImage = document.createElement("img")
-            // Replace the original element with the new one
-            slideSource.parentNode.replaceChild(slideChangeToImage, slideSource)
-            slideChangeToImage.className = "slide_source"
-            slideChangeToImage.src = source
-            slideChangeToImage.alt = title
-        } else {
-            // Get the original element
-            const slideSource = document.querySelector(".slide_source")
-            // Create a new element with a video tag name
-            const slideChangeToVideo = document.createElement("video")
-            // Replace the original element with the new one
-            slideSource.parentNode.replaceChild(slideChangeToVideo, slideSource)
-            slideChangeToVideo.className = "slide_source"
-            slideChangeToVideo.src = source
-            slideChangeToVideo.controls = true            
-        }        
-        const slideTitle = document.querySelector(".slide_title")       
-        slideTitle.textContent = title
+    // Set the image source and title based on the current image index
+    if (isImage === 'true') {
+      // Get the original element
+      const slideSource = document.querySelector('.slide_source')
+      // Create a new element with a img tag name
+      const slideChangeToImage = document.createElement('img')
+      // Replace the original element with the new one
+      slideSource.parentNode.replaceChild(slideChangeToImage, slideSource)
+      slideChangeToImage.className = 'slide_source'
+      slideChangeToImage.src = source
+      slideChangeToImage.alt = title
+    } else {
+      // Get the original element
+      const slideSource = document.querySelector('.slide_source')
+      // Create a new element with a video tag name
+      const slideChangeToVideo = document.createElement('video')
+      // Replace the original element with the new one
+      slideSource.parentNode.replaceChild(slideChangeToVideo, slideSource)
+      slideChangeToVideo.className = 'slide_source'
+      slideChangeToVideo.src = source
+      slideChangeToVideo.controls = true
     }
+    const slideTitle = document.querySelector('.slide_title')
+    slideTitle.textContent = title
+  }
 }
 
 class ImageMedia extends Medias {
-    constructor(id, photographerId, title, source, likes, date, price) {
-        super(id, photographerId, title, source, likes, date, price);
-    }  
-    
-    getSource() {
-        const mediaImage = document.createElement("img")
-        mediaImage.className = "media_source"
-        mediaImage.src = this.source
-        mediaImage.alt = this.title
+  // eslint-disable-next-line no-useless-constructor
+  constructor (id, photographerId, title, source, likes, date, price) {
+    super(id, photographerId, title, source, likes, date, price)
+  }
 
-        return mediaImage
-    }
+  getSource () {
+    const mediaImage = document.createElement('img')
+    mediaImage.className = 'media_source'
+    mediaImage.src = this.source
+    mediaImage.alt = this.title
 
-    getLightboxSource() {
-        const imageSource = document.createElement("img")
-        imageSource.className = "slide_source"
-        imageSource.src = this.source
-        imageSource.alt = this.title
+    return mediaImage
+  }
 
-        return imageSource
-    }
+  getLightboxSource () {
+    const imageSource = document.createElement('img')
+    imageSource.className = 'slide_source'
+    imageSource.src = this.source
+    imageSource.alt = this.title
+
+    return imageSource
+  }
 }
 
 class VideoMedia extends Medias {
-    constructor(id, photographerId, title, source, likes, date, price) {
-        super(id, photographerId, title, source, likes, date, price);
-    }
+  // eslint-disable-next-line no-useless-constructor
+  constructor (id, photographerId, title, source, likes, date, price) {
+    super(id, photographerId, title, source, likes, date, price)
+  }
 
-    getSource() {
-         const mediaVideo = document.createElement("video")
-         mediaVideo.className = "media_source"
-         mediaVideo.controls = true
-         mediaVideo.src = this.source
+  getSource () {
+    const mediaVideo = document.createElement('video')
+    mediaVideo.className = 'media_source'
+    mediaVideo.controls = true
+    mediaVideo.src = this.source
 
-         return mediaVideo
-    }
+    return mediaVideo
+  }
 
-    getLightboxSource() {
-        const videoSource = document.createElement("video")
-        videoSource.className = "slide_source"
-        videoSource.controls = true
-        videoSource.src = this.source
+  getLightboxSource () {
+    const videoSource = document.createElement('video')
+    videoSource.className = 'slide_source'
+    videoSource.controls = true
+    videoSource.src = this.source
 
-        return videoSource
-    }
+    return videoSource
+  }
 }
 
 export {
-    ImageMedia,
-    VideoMedia
+  ImageMedia,
+  VideoMedia
 }
--- src/scripts/pages/photographer.js
+++ src/scripts/pages/photographer.js
@@ -1,9 +1,9 @@
-import getData from "../../scripts/services/data.services.js"
-import Photographer from "../../scripts/models/Photographer.js"
-import MediaFactory from "../../scripts/factories/MediasFactory.js"
+import getData from '../../scripts/services/data.services.js'
+import Photographer from '../../scripts/models/Photographer.js'
+import MediaFactory from '../../scripts/factories/MediasFactory.js'
 
-let getPhotographerId = () => {
-    return parseInt(new URL(window.location.href).searchParams.get("id"), 10)
+const getPhotographerId = () => {
+  return parseInt(new URL(window.location.href).searchParams.get('id'), 10)
 }
 
 const photographerId = getPhotographerId()
@@ -12,124 +12,118 @@ const factory = new MediaFactory()
 
 displayPhotographerPage(data)
 
-function displayPhotographerPage(data) {    
-    const photographers = data.photographers    
-    const photographerInfo = photographers.find((p) => p.id === photographerId)
-    const medias = data.medias
-    const mediasInfo = medias.filter((m) => m.photographerId === photographerId)
-    const mediasLikes = mediasInfo.map((l) => l.likes)
+function displayPhotographerPage (data) {
+  const photographers = data.photographers
+  const photographerInfo = photographers.find((p) => p.id === photographerId)
+  const medias = data.medias
+  const mediasInfo = medias.filter((m) => m.photographerId === photographerId)
+  const mediasLikes = mediasInfo.map((l) => l.likes)
 
-    // Use reduce() method to find the total of likes
-    let totalOfLikes = mediasLikes.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
-    },0)
+  // Use reduce() method to find the total of likes
+  const totalOfLikes = mediasLikes.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, 0)
 
-    let photographer = new Photographer(
-        photographerInfo.name,
-        photographerInfo.id,
-        photographerInfo.city,
-        photographerInfo.country,
-        photographerInfo.tagline,
-        photographerInfo.price,
-        photographerInfo.portrait,
-        totalOfLikes
+  const photographer = new Photographer(
+    photographerInfo.name,
+    photographerInfo.id,
+    photographerInfo.city,
+    photographerInfo.country,
+    photographerInfo.tagline,
+    photographerInfo.price,
+    photographerInfo.portrait,
+    totalOfLikes
 
+  )
+  photographer.displayHeaderInfo()
+  photographer.displayAside()
+  displayGalley(mediasInfo)
+}
+
+function displayGalley (mediasInfo) {
+  // eslint-disable-next-line prefer-const
+  for (let info of mediasInfo) {
+    const media = factory.buildMedia(
+      info.id,
+      info.photographerId,
+      info.title,
+      info.image ? `../../src/assets/medias/${info.image}` : `../../src/assets/medias/${info.video}`,
+      info.likes,
+      info.date,
+      info.price,
+      info.image ? 'image' : 'video'
     )
-    photographer.displayHeaderInfo()
-    photographer.displayAside()
-    displayGalley(mediasInfo)    
+    media.displayMedias()
+  }
+  // Add default filter - popularity
+  sortMedias()
 }
 
-function displayGalley(mediasInfo) {
-    for(let info of mediasInfo) {
-        let media = factory.buildMedia(
-            info.id,
-            info.photographerId,
-            info.title,
-            info.image ? `../../src/assets/medias/${info.image}` : `../../src/assets/medias/${info.video}`,
-            info.likes,
-            info.date,
-            info.price,
-            info.image ? 'image' : 'video'
-        )
-        media.displayMedias()
-    }
-    // Add default filter - popularity
+function filterGallery () {
+  const filter = document.getElementById('filter')
+
+  // Launch filter event
+  filter.addEventListener('click', (e) => {
+    e.preventDefault()
     sortMedias()
-}
-
-function filterGallery() {
-    const filter = document.getElementById("filter")
-
-    // Launch filter event
-    filter.addEventListener("click", (e) => {
-        e.preventDefault()
-        sortMedias()   
-    })
-
-    // Launch filter event with enter key
-    // filter.addEventListener('keydown', (e) => {
-    //     e.preventDefault()
-    //     if (e.key === 'Enter') {
-    //         sortMedias() 
-    //     }                 
-    // })
+  })
 }
 filterGallery()
 
-function sortMedias() {
-    const option = filter.options[filter.selectedIndex].value  
-    // Get the list of medias from the DOM
-    const gallery = document.querySelector(".photographer_gallery_medias")  
-    const items = gallery.childNodes    
-    const itemsList = []
+function sortMedias () {
+  const option = filter.options[filter.selectedIndex].value
+  // Get the list of medias from the DOM
+  const gallery = document.querySelector('.photographer_gallery_medias')
+  const items = gallery.childNodes
+  const itemsList = []
 
-    for (let i in items) {
-        if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-            itemsList.push(items[i])
-        }
+  // eslint-disable-next-line prefer-const
+  for (let i in items) {
+    // get rid of the whitespace text nodes
+    if (items[i].nodeType === 1) {
+      itemsList.push(items[i])
     }
+  }
 
-    itemsList.sort(function(a, b) {
-        switch(option) {
-            case "title":
-                return (a.getAttribute('data-title') < b.getAttribute('data-title')) ? -1 : 1
-    
-            case "date":
-                return (b.getAttribute('data-date') < a.getAttribute('data-date')) ? -1 : 1
-    
-            default:
-                return (parseInt(b.getAttribute('data-likes')) < parseInt(a.getAttribute('data-likes'))) ? -1 : 1
-        }
-    })
+  itemsList.sort(function (a, b) {
+    switch (option) {
+      case 'title':
+        return (a.getAttribute('data-title') < b.getAttribute('data-title')) ? -1 : 1
 
-    for (let i = 0; i < itemsList.length; ++i) {
-        gallery.appendChild(itemsList[i])
+      case 'date':
+        return (b.getAttribute('data-date') < a.getAttribute('data-date')) ? -1 : 1
+
+      default:
+        return (parseInt(b.getAttribute('data-likes')) < parseInt(a.getAttribute('data-likes'))) ? -1 : 1
     }
+  })
+
+  for (let i = 0; i < itemsList.length; ++i) {
+    gallery.appendChild(itemsList[i])
+  }
 }
 
 // HANDLE LIKES //
-const totalOfLikes = document.querySelector(".total_of_likes")
-const likesIcon = document.querySelectorAll(".likes_icon")
+const totalOfLikes = document.querySelector('.total_of_likes')
+const likesIcon = document.querySelectorAll('.likes_icon')
 
 // Launch add likes event
-likesIcon.forEach((icon) => icon.addEventListener("click", (e) => {
-        e.preventDefault()
-    addLike(e)          
+likesIcon.forEach((icon) => icon.addEventListener('click', (e) => {
+  e.preventDefault()
+  addLike(e)
 }))
 
 // Launch add likes event with enter key
-likesIcon.forEach((icon) => icon.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-        addLike(e)
-    }                 
+likesIcon.forEach((icon) => icon.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addLike(e)
+  }
 }))
 
-function addLike(e) {
-    if (e.currentTarget.getAttribute('data-liked') === "unliked") {
-        console.log("hello if")
-        e.currentTarget.previousSibling.textContent++
-        totalOfLikes.textContent++
-        e.currentTarget.setAttribute("data-liked", "liked")
-    }
+function addLike (e) {
+  if (e.currentTarget.getAttribute('data-liked') === 'unliked') {
+    e.currentTarget.previousSibling.textContent++
+    totalOfLikes.textContent++
+    e.currentTarget.setAttribute('data-liked', 'liked')
+  }
 }
